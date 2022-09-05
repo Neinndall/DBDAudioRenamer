fb19143d6db32c83b818dec13fb2f7f7b1589181
Th3Nigh7mare <66384195+Th3Nigh7mare@users.noreply.github.com>
2022-09-05 15:48:58 +0200

    Update
    some slight changes to add another .bnk file extractor to improve efficiency in the future

--- Run_This.bat
+++ Run_This.bat
@@ -1,13 +1,12 @@
-"Tools\quickbms.exe" "Tools\Wwise.bms" "Files\*.bnk" "Files"
-pause
-mkdir "Files\Decoding"
-pause
-"Tools\quickbms.exe" "Tools\Wwise_parse.bms" "Files\*.xml" "Files\Decoding"
-pause
-for /r %%f in (*.wem) do ".\ww2ogg\ww2ogg.exe" "%%f" --pcb ".\ww2ogg\packed_codebooks_aoTuV_603.bin"
-pause
-
 @echo off
-echo. -------------------------------------------------------------
-echo. Unpack finished! Files will be in the 'Files/decoding' folder
-echo. -------------------------------------------------------------
+title DBD-Extractor v0.1.1 (BNK Files and XML Files) by Th3Nigh7mare
+color b
+for /r %%d in (*.bnk) do ".\Tools\bnkextr.exe" "%%d" /nodir
+pause
+mkdir "Files\decoding"
+pause
+"Tools\quickbms.exe" "Tools\scripts\Wwise_parse.bms" "Files\*.xml" "Files\decoding"
+pause
+for /r %%d in (*.wem) do ".\ww2ogg\ww2ogg.exe" "%%d" --pcb ".\ww2ogg\packed_codebooks_aoTuV_603.bin"
+echo. Unpack and parser finished! The files are in Files/decoding
+pause
